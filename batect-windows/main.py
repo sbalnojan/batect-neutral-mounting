075f5e57970e85b67d6291115f0cfa712d8d7d6d
Sven Balnojan <sven.balnojan@mercateo.com>
2021-10-28 08:27:24 +0200

    add windows base

---
+++ batect-windows/main.py
@@ -0,0 +1,9 @@
+
+
+if __name__ == "__main__":
+    
+    print("This is just an empty main script checking that \
+    your dev environment actually works by importing some of \
+    the requirements....\n testing...")
+    import sqlalchemy
+    print("Perfect, if you see no errors, it works!")
